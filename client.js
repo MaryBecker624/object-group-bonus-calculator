57c058d97a17d5e50a03f0051d58f9c4b76b69b8
abgeiger <30698092+abgeiger@users.noreply.github.com>
2017-11-07 13:54:04 -0600

    completed assignment

--- client.js
+++ client.js
@@ -10,3 +10,55 @@ var employees = [ atticus, jem, boo, scout, robert, mayella ];
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
 console.log(employees);
+
+function EmployeeObject(oldObject) {
+    this.name = oldObject.name;
+    this.bonusPercentage = bonusPercentageCreator(oldObject.reviewRating, oldObject.employeeNumber, 
+        oldObject.annualSalary);
+    this.totalBonus = totalBonusCreator(oldObject.annualSalary, this.bonusPercentage);
+    this.totalCompensation = totalCompensationCreator(oldObject.annualSalary, this.totalBonus);
+}
+
+function bonusPercentageCreator(currentRating, currentEmployeeNumber, currentAnnualIncome) {
+    var bonusPercentage;
+    if (currentRating <= 2) {
+        bonusPercentage = 0;
+    } else if (currentRating ==3) {
+        bonusPercentage = 0.04;
+    } else if (currentRating ==4) {
+        bonusPercentage = 0.06;
+    } else if (currentRating ==5) {
+        bonusPercentage = 0.1;
+    } 
+    if (currentEmployeeNumber.length ==4){
+        bonusPercentage += 0.05;
+    }
+    if (currentAnnualIncome>65000) {
+        bonusPercentage-=0.01;
+    }
+    if (bonusPercentage > 0.13) {
+        bonusPercentage = 0.13;
+    }
+    if (bonusPercentage<0) {
+        bonusPercentage = 0;
+    }
+    return bonusPercentage;
+}
+
+function totalBonusCreator(currentAnnualIncome, currentBonusPercentage){
+    return Math.floor(parseInt(currentAnnualIncome) * currentBonusPercentage);
+}
+
+function totalCompensationCreator (currentAnnualIncome, currentTotalBonus){
+    return (parseInt(currentAnnualIncome) + currentTotalBonus);
+
+}
+
+function logEmployeeObjects(array){
+    for (var i=0; i<array.length; i++){
+        var output = new EmployeeObject(array[i]);
+        console.log(output);
+    }
+}
+
+logEmployeeObjects(employees);
